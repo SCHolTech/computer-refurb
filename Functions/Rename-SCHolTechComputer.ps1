add0bf0c82e4eaac4c513756d72e55300f37647c
Carl Molyneux <carl@cm-cs.co.uk>
2022-07-04 09:43:05 +0100

    Clarity on enter ID message

--- Functions/Rename-SCHolTechComputer.ps1
+++ Functions/Rename-SCHolTechComputer.ps1
@@ -3,7 +3,7 @@ function Rename-SCHolTechComputer {
 	$computerName = $env:computername;
 	if(-not $computerName.StartsWith("SCHOL-T")){
 		Write-Host "Computer needs renaming!"
-		[int]$computerId = Read-Host -Prompt "Please enter the computer ID"
+		[int]$computerId = Read-Host -Prompt "Please enter the computer ID. e.g. if the T number is T68, enter 68"
 		$newComputerName = "SCHOL-T$computerId"
 		$ok = Read-Host -Prompt "The computer name will be set to $newComputerName. Hit y to continue"
 		if($ok.ToLower() -eq "y") {
